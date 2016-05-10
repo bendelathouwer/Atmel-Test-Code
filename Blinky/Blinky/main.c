675d7dcba2cd09a4682e9aebb8283c429db2c748
Ben <Bendelathouwer@gmail.com>
2016-05-10 18:55:12 +0200

    blinky
    Blinky code added

---
+++ Blinky/Blinky/main.c
@@ -0,0 +1,19 @@
+/*
+ * Blinky.c
+ *
+ * Created: 10/05/2016 18:53:51
+ * Author : Ben
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
