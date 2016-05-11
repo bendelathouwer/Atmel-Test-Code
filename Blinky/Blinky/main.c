736edc39143a155713c4749dda37abcbc7e815e3
Ben <Bendelathouwer@gmail.com>
2016-05-11 19:24:34 +0200

    Started
    Starded agian becous programmer needs to be added first

--- Blinky/Blinky/main.c
+++ Blinky/Blinky/main.c
@@ -2,18 +2,63 @@
  * Blinky.c
  *
  * Created: 10/05/2016 18:53:51
- * Author : Ben
- */ 
+ * Author : Ben 
+  */ 
+
+
+#define F_CPU 20000000UL// clock freuquentie of the chip we use 
 
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
+
+//function defenition 
+void blink (void);
+
+void ReadInput (void);
+
+
+// Pin 13 on arduino Board 
+#define  LED PD7
+
+
+
+
+
+// easy code to write to a pin  
+#define output_low(port,pin) port &= ~(1<<pin)
+#define output_high(port,pin) port |= (1<<pin)
+
+// easy code for the direction of the pin
+#define set_input(portdir,pin) portdir &= ~(1<<pin)
+#define set_output(portdir,pin) portdir |= (1<<pin)
+
 
 
 int main(void)
 {
-    /* Replace with your application code */
+    set_output(DDRD,LED);
+	//set_input(DDRD,BUTTON);
     while (1) 
     {
+		blink ();
+		
     }
+	return 0 ;
 }
 
+
+void blink(void)
+{
+	output_high(PORTD,LED);// 
+	_delay_ms(200);
+	output_low(PORTD,LED);
+	_delay_ms(200);
+	
+}
+/*
+void ReadInput(void)
+{
+
+}
+
+*/
---
+++ atmel snipsets/pin settings.c
@@ -0,0 +1,5 @@
+// "makelijk" makende macro's
+#define output_low(port,pin) port &= ~(1<<pin)
+#define output_high(port,pin) port |= (1<<pin)
+#define set_input(portdir,pin) portdir &= ~(1<<pin)
+#define set_output(portdir,pin) portdir |= (1<<pin)
